eb914f70f10ecae7129de24db9713491793c25a0
webdesigener-ashraf <ashrafrahmani9837@gmail.com>
2025-11-11 07:37:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const image = document.querySelector("#image");
-const preview = document.querySelectorAll(".preview");
-preview.forEach(function(preview) {
-preview.addEventListener("mouseover",function(){
+const previews = document.querySelectorAll(".preview");
+
+previews.forEach(function(preview) {
+  preview.addEventListener("mouseover", function() {
     image.style.backgroundImage = `url(${this.src})`;
-    image.innerHTML=this.alt;
-})
-});
-preview.addEventListener("mouseout",function(){
-    image.style.backgroundImage= "none";
-    image.innerHTML="Hover over an image below to display here."
+    image.innerHTML = this.alt;
+  });
+
+  preview.addEventListener("mouseout", function() {
+    image.style.backgroundImage = "none";
+    image.innerHTML = "Hover over an image below to display here.";
+  });
 });
